89e2654bb8cebafe819a5485c09972a8dd918341
12thDimension <leon.buchal@web.de>
2023-01-04 00:09:25 +0100

    all

--- core/src/com/mygdx/elgame/ELGame.java
+++ core/src/com/mygdx/elgame/ELGame.java
@@ -1,31 +1,39 @@
 package com.mygdx.elgame;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.GL20;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ELGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	ShapeRenderer shape;
+	ArrayList<Ball> balls = new ArrayList<>();
+	Paddle paddle = new Paddle(120);
+	Random r = new Random();
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void create() {
+		shape = new ShapeRenderer();
+		for (int i = 0; i < 1; i++) {
+			balls.add(new Ball(400,
+					300,
+					40, 0, 0));
+		}
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+
+		for (Ball ball : balls) {
+			ball.update();
+			ball.draw(shape);
+		}
+		paddle.draw(shape);
+		shape.end();
 	}
 }
